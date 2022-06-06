60fe9c17d93cad3cd7f8dd6d626b80e847a5c080
DiscoDurodeRoer <30374642+DiscoDurodeRoer@users.noreply.github.com>
2022-06-06 20:01:49 +0200

    example drag and drop

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DragDropComponent } from './drag-drop/drag-drop.component';
+
+@NgModule({
+  declarations: [	
+    AppComponent,
+    DragDropComponent
+   ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    DragDropModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
